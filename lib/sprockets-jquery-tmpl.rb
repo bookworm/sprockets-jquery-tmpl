9eac49c8d4e6b126e99c44bf999064e32546d7e2
Ken Erickson <bookworm.productions@gmail.com>
2011-11-02 11:44:10 -0700

    remove .rmvc and add padrino support

--- lib/sprockets-jquery-tmpl.rb
+++ lib/sprockets-jquery-tmpl.rb
@@ -1,3 +1,3 @@
 require 'sprockets/jquery/tmpl/version'
 require 'sprockets/jquery/tmpl/tmpl_processor'
-require 'sprockets/jquery/tmpl/engine'
+require 'sprockets/jquery/tmpl/engine' if defined?(Rails)
--- lib/sprockets/jquery/tmpl/tmpl_processor.rb
+++ lib/sprockets/jquery/tmpl/tmpl_processor.rb
@@ -1,14 +1,17 @@
 require 'sprockets/engines'
-require 'tilt'
-require 'action_view'
-require 'action_view/helpers'
-require 'action_view/helpers/javascript_helper'
+require 'tilt'  
+    
+if defined?(Rails)
+  require 'action_view'
+  require 'action_view/helpers'
+  require 'action_view/helpers/javascript_helper'  
+end
 
 module Sprockets
   module Jquery
     module Tmpl
       class TmplProcessor < Tilt::Template
-        include ActionView::Helpers::JavaScriptHelper
+        include ActionView::Helpers::JavaScriptHelper if defined?(Rails)  
 
         def self.default_mime_type
           'application/javascript'
@@ -17,12 +20,20 @@ module Sprockets
         def prepare
         end
 
-        def evaluate(scope, locals, &block)
-          <<-TMPL
+        def evaluate(scope, locals, &block)     
+          if defined?(Rails)
+            <<-TMPL
 (function($) {
   $.template(#{scope.logical_path.gsub(/^tmpls\/(.*)$/i, "\\1").inspect}, "#{escape_javascript data}");
-})(jQuery);
-          TMPL
+})(jQuery);   
+            TMPL
+          elsif defined?(Padrino)
+            <<-TMPL
+(function($) {
+  $.template(#{scope.logical_path.gsub(/^tmpls\/(.*)$/i, "\\1").inspect}, "#{js_escape_html data}");
+})(jQuery);   
+            TMPL
+          end
         end
       end
     end
