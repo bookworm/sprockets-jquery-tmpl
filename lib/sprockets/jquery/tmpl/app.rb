d281d809fbf93f8ebda304ba894138d69e6b0ace
Ken Erickson <bookworm.productions@gmail.com>
2012-03-24 22:42:19 -0700

    append correct path to sprockets env

--- lib/sprockets/jquery/tmpl/app.rb
+++ lib/sprockets/jquery/tmpl/app.rb
@@ -3,7 +3,7 @@ module Sprockets
     module Tmpl    
       module App
         def self.registered(app)     
-          app.sprockets.append_path(File.expand_path('../app/assets', __FILE__))
+          app.sprockets.append_path(File.expand_path('../../../../../app/assets/javascripts', __FILE__)) 
         end      
       end
     end
